5e54d024a781c5bc9896400a678283b83f400ea0
rs280 <rishabhsahoo1@gmail.com>
2022-04-07 22:27:20 -0700

    first commit

---
+++ Week1/PrintManyShapesUsingDifferentChars.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout << "Triangle:" << endl;
+    cout << "     *" << endl;
+    cout << "    ***" << endl;
+    cout << "   *****" << endl;
+    cout << "Square:" << endl;
+    cout << "   @@@" << endl;
+    cout << "   @ @" << endl;
+    cout << "   @@@" << endl;
+    cout << "Rectangle:" << endl;
+    cout << "   #####" << endl;
+    cout << "   #   #" << endl;
+    cout << "   #####" << endl;
+    cout << "Diamond:" << endl;
+    cout << "    %" << endl;
+    cout << "   % %" << endl;
+    cout << "    %" << endl;
+    return 0;
+}
---
+++ Week1/helloworld.cpp
@@ -0,0 +1,36 @@
+/*
+C++ syntax
+#include : preproccessor directive: incude thish header file first
+
+using namespace std; : use the standard namespace
+
+main() function to run the program
+
+C++ syntax is used to write programs in C++, so C++ compile can transalte to machine readable code.
+
+cout is the stnadard output stream, which is used to output data to the screen.
+
+<< is the stream insertion operator, which is used to insert data into the stream.
+
+endl is the end of line, which is used to end the line.
+
+return 0 means that this function will return 0 to the caller. 
+
+C++ is case sensitive, so the variable name must be written in lower case.
+
+*/
+
+
+
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    cout << "Hello World" << endl;
+
+    cout <<"Good bye world";
+
+    cout <<"Thank you world" << endl;
+ 
+    return 0;
+}
