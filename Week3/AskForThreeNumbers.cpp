12105882030577867a80205ac65d253164d9eda6
rs280 <rishabhsahoo1@gmail.com>
2022-04-23 14:40:51 -0700

    changing gitignore, and submitted 1st week 2 assignemtn

--- Week3/AskForThreeNumbers.cpp
+++ Week3/AskForThreeNumbers.cpp
@@ -1,53 +1,51 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    double first = 0;
-    double second = 0;
-    double third = 0;
-    do{
-        cout << "Please enter the first number: ";
-        cin >> first;
-        if (cin.fail()==true){
-            cout << "Invalid number. Please try again." << endl;
-            cin.clear();
-            cin.ignore(INT_MAX);}
-        if (first<=0){
-            cout << "The first number must be greater than 0. Please try again." << endl;
-            cin.clear();
-            cin.ignore(INT_MAX, '\n'); }
-        
-            
-        
-        
-    } while ((first<=0)||(cin.fail()==true));
-    
-    do {
-        cout << "Please enter the second number: ";
-        cin >> second;
-        if(cin.fail()==true){
-            cout <<"Invalid number. Please try again." << endl;
-            cin.clear();
-            cin.ignore(INT_MAX , '\n'); }
-        if(second<=first){
-            cout << "The second number must be greater than the first number. Please try again." << endl;
-            cin.clear();
-            cin.ignore(INT_MAX, '\n'); }
-    } while ((second<=first)||(cin.fail() == true));
+int main(){
+    /* read in 3 different numbers with the following rules:
+- the first number must be greater than 0
+- the second number must be greater than the first number
+- the third number must be between the first number and the third number and not equal.*/
+    double num1, num2, num3;
+   do{
+       cout<<"Please enter the first number: ";
+         cin>>num1;
+         if (cin.fail()==true){
+                cin.clear();
+                cin.ignore(INT_MAX,'\n');
+                cout<<"Invalid input. Please try again"<<endl;
+         }
+         else if(num1<=0){
+             cout<<"The first number must be greater than 0. Please try again"<<endl;
+         }
 
-    do {
-        cout << "Please enter the third number: ";
-        cin >> third;
-        if ((first>=third)||(second<=third)){
-            cout << "The third number must be greater than the first and second number. Please try again." << endl;
-            cin.clear();
-            cin.ignore(INT_MAX, '\n'); }
-        else if (cin.fail()==true){
-            cout << "Invalid number. Please try again." << endl;
-            cin.clear();
-            cin.ignore(INT_MAX); }
-        
-    } while ((first>=third)||(second<=third)||(cin.fail() == true));
-    
+   }while ((num1<=0)||(cin.fail()==true));
+
+    do{
+    cout<<"Please enter the second number: ";
+    cin>>num2;
+    if (cin.fail()==true){
+                cin.clear();
+                cin.ignore(INT_MAX,'\n');
+                cout<<"Invalid input. Please try again"<<endl;
+        }
+        else if(num2<=num1){
+            cout<<"The second number must be greater than the first number. Please try again"<<endl;
+        }
+
+    }while((num2<=num1)||(cin.fail()==true));
+
+    do{
+    cout<<"Please enter the third number: ";
+    cin>>num3;
+    if (cin.fail()==true){
+                cin.clear();
+                cin.ignore(INT_MAX,'\n');
+                cout<<"Invalid input. Please try again"<<endl;
+        }
+        else if(num3<=num1||num3>=num2){
+            cout<<"The third number must be between the first number and the second number and not equal. Please try again"<<endl;
+        }
+    } while((num3<=num1||num3>=num2)||(cin.fail()==true));
+    cout << "The three numbers you have entered: " << num1 <<", "<< num2<< ", "<< num3<< endl;
     return 0;
 }
---
+++ Week3/HowToUseFunctions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/*
+function: perform a task
+ - break into smaller tasks
+ function definition:
+    - function name
+    - parameters
+    - return type
+         voind : no return value*/
+
+int main(){
+    for (int count=0; count < 5 ; count++){
+        cout << "Hello World" << endl;
+    }
+}
