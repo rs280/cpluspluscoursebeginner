d6542d54e6a61d8b33d11695bdc590ad0234dc78
rs280 <rishabhsahoo1@gmail.com>
2022-04-25 19:17:51 -0700

    finished week 3 assignments

--- Week3/ReadAndConfirmPIN.cpp
+++ Week3/ReadAndConfirmPIN.cpp
@@ -12,21 +12,34 @@ int main(){
     int TESTPIN;
     int PIN;
     int count=0;
-
+    int wrongcount=0;
     cout<<"Please enter your PIN: ";
     cin>>PIN;
-    while(count<=3){
-        cout<<"Please enter the PIN again to confirm: "<<endl;
-            cin>>TESTPIN;
+    while((count<3)){
+        cout<<"Please enter the PIN again to confirm: ";
+        cin>>TESTPIN;
       if (matchinput(TESTPIN,PIN)){
           count++;
+          break;
       }
       else if (matchinput(TESTPIN,PIN)==false){
           cout<<"Mismatched. Please try again."<<endl;
           count++;
+      }}
+
+      
+      if (count<=3){
+          cout<<"Accepted"<<endl;
+          cout<<count<<" attempts."<<endl;
+      }
+      else {
+          cout<<"Mismatched. Too many unsuccessful tries."<<endl;
+
+            cout<<"Not Accepted."<<endl;
+           cout<<"3 attempts."<<endl;
       }
       
-    }
+    
 
     return 0;
 }
